59944c90170f46da4a6f20b3cdc95cfca3f07fda
MohamedkamalSoudi <mk0875734@gmail.com>
2025-04-19 10:14:09 +0200

    add app colors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color black = Color(0xFF000000);
+  static const Color grey60= Color(0xFF4B4B4B);
+  static const Color whitebody = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFEF4444);
+  static const Color green = Color(0xFF22C55E);
+  static const Color blue = Color(0xFF4DADFB);
+  static const Color grey100 = Color(0xFF7C7C7C);
+  static const Color grey200 = Color(0xFFCCC3C3);
+  static const Color grey300 = Color(0xFF707070);
+  static const Color cardDescription = Color(0xFF809CFF);
+  static const Color iconprofile = Color(0xFF8696BB);
+  static const Color iconhome = Color(0xFF63B4FF);
+
+
+}
