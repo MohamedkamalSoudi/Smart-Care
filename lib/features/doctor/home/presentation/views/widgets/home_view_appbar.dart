2be47a1739904481a7424e14aa1312b9153adad7
MohamedkamalSoudi <mk0875734@gmail.com>
2025-04-19 10:24:00 +0200

    edit color app bar home viwe

--- lib/features/doctor/home/presentation/views/widgets/home_view_appbar.dart
+++ lib/features/doctor/home/presentation/views/widgets/home_view_appbar.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../core/app_colors.dart';
+
 AppBar homeViewAppbar({bool isNurse = false, void Function()? onPressed}) {
   return AppBar(
+    scrolledUnderElevation: 0,
     centerTitle: true,
-    title: Text('Patients List', style: TextStyle(color: Colors.black)),
-    backgroundColor: Colors.white,
+    title: Text('Patients List', style: TextStyle(color: AppColors.black)),
+    backgroundColor: AppColors.whitebody,
     elevation: 0,
     actions: isNurse
-        ? [IconButton(onPressed: onPressed, icon: Icon(Icons.notifications))]
+        ? [IconButton(onPressed: onPressed, icon: Icon(Icons.notifications , color: AppColors.black))]
         : [],
     automaticallyImplyLeading: false,
   );
