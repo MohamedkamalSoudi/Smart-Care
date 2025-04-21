e5653d8653effa140bb9252febc00da461e69358
moRagabmo12 <moragabmohamed12@gmail.com>
2025-04-21 14:18:40 +0200

    create assets file

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,13 @@
+class AssetsData {
+  static const logo = "assets/images/logo.png";
+  static const bitmap = "assets/images/bitmap.png";
+  static const mandoctor = "assets/images/man_doctor.png";
+  static const womandoctor = "assets/images/woman_doctor.png";
+  static const editIcon = "assets/images/editIcon.png";
+  static const flask = "assets/images/Flask.png";
+  static const lungs = "assets/images/Lungs.png";
+  static const patientandbed = "assets/images/patientin_ward_with_doctors.png";
+  static const policy = "assets/images/Policy.png";
+  static const vitamin = "assets/images/Vitamin.png";
+  static const vector = "assets/images/Vector.svg";
+}
