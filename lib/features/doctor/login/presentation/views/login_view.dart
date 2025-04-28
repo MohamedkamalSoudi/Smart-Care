dbca1990166cd12641d3a49c10dc8c7bd72de549
moRagabmo12 <moragabmohamed12@gmail.com>
2025-04-28 23:40:35 +0300

    done

--- lib/features/doctor/login/presentation/views/login_view.dart
+++ lib/features/doctor/login/presentation/views/login_view.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_care_app/core/utils/widgets/custom_buttom.dart';
