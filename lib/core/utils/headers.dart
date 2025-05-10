1a4e976cc6e14bff78f3c74b59fa9c429a915aa3
moRagabmo12 <moragabmohamed12@gmail.com>
2025-05-10 12:36:23 +0300

    change cookie

--- lib/core/utils/headers.dart
+++ lib/core/utils/headers.dart
@@ -1,9 +1,9 @@
 class HeadersApi {
   static const String contentType = 'application/json';
   static const String accept = 'application/json';
-  static const String cookie = '__test=688dbdd15cfa2c95eb17c60f1bc4a72b';
+  static const String cookie = '__test=eb401a96ad9db023592f3fd4acc21787';
   static const String userAgent =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0';
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36';
   static const String authorization =
       'Bearer 1|D9uDVgreerqXaeUIlJH7ENvHPAYoX66j3w08qi8jab248552';
 }
