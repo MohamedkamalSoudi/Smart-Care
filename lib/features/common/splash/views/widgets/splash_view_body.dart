04e83b5f3665659e633e91ba82248720b3a8c85c
Ziad <zslah2800@gmail.com>
2025-05-23 01:06:36 +0300

    adding constant file and token variable

--- lib/features/common/splash/views/widgets/splash_view_body.dart
+++ lib/features/common/splash/views/widgets/splash_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smart_care_app/constant.dart';
+import 'package:smart_care_app/features/doctor/home/presentation/views/home_view_doctor.dart';
 import '../../../login/presentation/views/login_view.dart';
 import 'sliding_text.dart';
 import '../../../onboarding/presentation/views/onboarding_view.dart';
@@ -58,7 +60,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
       final response = sharedPref.getBool('isClicked');
       response != true
           ? Navigator.pushReplacementNamed(context, OnboardingView.id)
-          : Navigator.pushReplacementNamed(context, LoginView.id);
+          : token == ''
+              ? Navigator.pushReplacementNamed(context, LoginView.id)
+              : Navigator.pushReplacementNamed(context, HomeViewDoctor.id);
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smart_care_app/constant.dart';
 import 'smart_care.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferences.getInstance();
+  final sharedPref = await SharedPreferences.getInstance();
+  token = sharedPref.getString('token') ?? '';
   runApp(const SmartCare());
 }
