5278d696829028dfe027e10bbbea1db57fed19f5
MohamedkamalSoudi <mk0875734@gmail.com>
2025-06-15 20:19:33 +0300

    edit logo app and edit name app

--- lib/core/utils/headers.dart
+++ lib/core/utils/headers.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class HeadersApi {
   static const String contentType = 'application/json';
   static const String accept = 'application/json';
-  static const String cookie = '__test=7218b20f89767d9d1585c9403e7678ee';
+  static const String cookie = '__test=66d8410eab3f68ea1b7df242a686f1a3';
   static const String userAgent =
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36';
 
