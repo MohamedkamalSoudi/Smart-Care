00d8ebf68e243f1be69d56678b21c5dcd5ad2947
MohamedkamalSoudi <mk0875734@gmail.com>
2025-06-18 08:08:09 +0300

    edit header

--- lib/core/utils/headers.dart
+++ lib/core/utils/headers.dart
@@ -3,9 +3,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 class HeadersApi {
   static const String contentType = 'application/json';
   static const String accept = 'application/json';
-  static const String cookie = '__test=f01d236c300bb9f1f21a779a2d7f6d21';
+  static const String cookie = '__test=f389f653040cd1baa3c3e0f8db7b28b8';
   static const String userAgent =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36';
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36';
 
   static Future<Map<String, String>> getHeaders() async {
     final prefs = await SharedPreferences.getInstance();
